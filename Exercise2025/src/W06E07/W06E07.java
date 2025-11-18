5876142c83c68a29e234ce4cf93ac36bf6bf1116
njxnj <njxnj@JingXun>
2025-11-18 15:49:14 +0800

    W06E07

--- Exercise2025/src/W06E07/W06E07.java
+++ Exercise2025/src/W06E07/W06E07.java
@@ -11,5 +11,6 @@ package W06E07;
 public class W06E07 {
     public static void main(String[] args) {
         System.out.println("Test Pull Request");
+        System.out.println("Test 2");
     }
 }
