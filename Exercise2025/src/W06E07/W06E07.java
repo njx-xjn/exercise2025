ba49141a33e463f83c00d7a9593d5d8280359c77
njxnj <njxnj@JingXun>
2025-11-18 16:48:21 +0800

    Test

--- Exercise2025/src/W06E07/W06E07.java
+++ Exercise2025/src/W06E07/W06E07.java
@@ -14,5 +14,6 @@ public class W06E07 {
         System.out.println("Test 2");
         System.out.println("Test 4");
         System.out.println("Test 5");
+        System.out.println("Test 6");
     }
 }
